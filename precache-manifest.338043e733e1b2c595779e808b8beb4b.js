7455dc399c675778225eeaf7146d34d725e17238
DESKTOP-GV7CGQ8\Mrolas <Nicholas.Michael.Ng@gmail.com>
2020-11-11 17:00:48 -0800

    Updates

---
+++ precache-manifest.338043e733e1b2c595779e808b8beb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28b6ddfb15d3ac4ae182420ada91621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fad15ce375a558bfabb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a24c7cfef9b088953006",
+    "url": "/static/js/2.a8dfad20.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.a8dfad20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fad15ce375a558bfabb",
+    "url": "/static/js/main.ae290e0f.chunk.js"
+  },
+  {
+    "revision": "69d2dc90ae5e442af17d",
+    "url": "/static/js/runtime-main.77c320f5.js"
+  }
+]);
